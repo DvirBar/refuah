106f12a2c056149138c408efdfb04369ac03d7a8
Dvir Bartov <dvirbartov@Dvirs-MacBook-Pro.local>
2021-08-18 09:01:34 +0300

    Initialize server and database

---
+++ server/src/app.ts
@@ -0,0 +1,30 @@
+import express from "express";
+import helmet from "helmet";
+import dotenv from "dotenv";
+import path from "path";
+import morgan from "morgan";
+import initMongoose from "./db/init";
+
+const app = express();
+app.use(helmet());
+
+// Setup environment variables
+dotenv.config({
+  path: path.resolve("env", `.env.${process.env.NODE_ENV}`),
+});
+
+// Middlewares
+app.use(express.json());
+app.use(morgan("tiny"));
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+
+// Create connection
+const port = process.env.PORT;
+// eslint-disable-next-line no-console
+app.listen(port, () => console.log(`Server started on port ${port}`));
+
+// Connect to MongoDB
+initMongoose();
---
+++ server/src/db/init.ts
@@ -0,0 +1,36 @@
+/* eslint-disable no-console */
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import path from "path";
+
+// Setup environment variables
+dotenv.config({
+  path: path.resolve("env", `.env.${process.env.NODE_ENV}`),
+});
+
+const {
+  MONGO_URI,
+  DB_NAME,
+  DB_USER,
+  DB_PASS,
+  DB_PORT,
+  DB_SERVICE,
+} = process.env;
+
+// Database config
+const db = MONGO_URI
+|| `mongodb://${DB_USER}:${DB_PASS}@${DB_SERVICE}:${DB_PORT}/${DB_NAME}?authSource=admin`;
+
+export default function initMongoose() {
+  console.log("Connecting to MongoDB...");
+
+  mongoose
+    .connect(db, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+    })
+    .then(() => console.log("MongoDB connected"))
+    .catch((err) => console.log(err));
+}
