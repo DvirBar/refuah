53736bfce16d018f13e1b3c94fc6eda778cf0d26
Dvir Bartov <dvirbartov@Dvirs-MacBook-Pro.local>
2021-08-28 17:32:49 +0300

    Setup client with react, typescript, eslint and storybook

---
+++ client/src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+
+function App(): JSX.Element {
+  return (
+    <div className="App">
+      Hello world
+    </div>
+  );
+}
+
+export default App;
